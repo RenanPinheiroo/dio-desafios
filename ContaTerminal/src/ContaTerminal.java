96625b7d120eb6a0ecb64b4bc8cc1e235870a5c5
Renan <renan.economia.2011@gmail.com>
2024-05-13 15:27:26 -0400

    atualização final

--- ContaTerminal/src/ContaTerminal.java
+++ ContaTerminal/src/ContaTerminal.java
@@ -1,14 +1,19 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class ContaTermial {
+public class ContaTerminal {
     public static void main(String[] args) {
+
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("Olá, seja bem vindo ao Banco Bradesco! Poderia, por gentileza nos informar:");
+        System.out.println("---------------------------------------------------------------------------");
+
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Qual é o seu nome?");
         String nomeCliente = scanner.nextLine();
-
+       
         System.out.println("Qual é o seu sobrenome?");
         String sobreNomeCliente = scanner.nextLine();
 
@@ -16,14 +21,14 @@ public class ContaTermial {
         int numero = scanner.nextInt();
 
         System.out.println("Qual é a sua Agência?");
-        String agencia = scanner.next();
+        String agencia = scanner.nextLine();
 
         double saldo = 237.48;
 
-        scanner.close();
+        
 
     System.out.println("Olá, Sr. "+ nomeCliente + " " + sobreNomeCliente + " . Você possui um saldo de" + saldo +  " na conta " + numero + "de Agência " + agencia);
 
-
+    scanner.close();
     }
 }
