ed9b14c32b7fdfe0fc22db2fb133b8b8c8d3cc94
Renan <renan.economia.2011@gmail.com>
2024-05-24 10:12:24 -0400

    Atualização com a nova array

--- controle-de-fluxo/src/Case2.java
+++ controle-de-fluxo/src/Case2.java
@@ -13,10 +13,9 @@ public class Case2 {
         while (NumeroFuncionarios < 5 && ProximoCandidato < candidatos.length) {
             String candidato = candidatos[ProximoCandidato++];
             double salary = Math.round(ValorPretendido());
-                System.out.println("O Funcionario " + candidato + " deseja ter um salario de " + salary);
+            System.out.println("O Funcionario " + candidato + " deseja ter um salario de " + salary);
 
-            if( salaryCia >= salary){
-
+            if(salaryCia >= salary){
                 System.out.println("Candidato Selecionado!");
                 selecionados.add(candidato);
                 NumeroFuncionarios++;
