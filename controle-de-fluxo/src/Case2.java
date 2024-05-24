4ed2b867db33cabd54e100f84e7a5c9a26d91927
Renan <renan.economia.2011@gmail.com>
2024-05-24 09:14:49 -0400

    Inclusao do salario random

--- controle-de-fluxo/src/Case2.java
+++ controle-de-fluxo/src/Case2.java
@@ -1,16 +1,31 @@
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Case2 {
     public static void main(String[] args) {
         String[] candidatos = getName();
+        double salary = Math.round(ValorPretendido());
+        double salaryCia = 2000;
+        int NumeroFuncionarios = 0;
+        int ProximoCandidato = 0 ;
+       
+        while (NumeroFuncionarios < 5 && ProximoCandidato < candidatos.length) {
+            String candidato = candidatos[ProximoCandidato++];
 
-        System.out.println(candidatos[4]);
-
+            System.out.println("O Funcionario " + candidato + " deseja ter um salario de " + salary);
+            
+        }
     }
-    
+
     static String[] getName(){
 
-        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","MIRELA","DANIELA","JORGE"};
         return candidatos;
+
+    }
+
+    static double ValorPretendido(){
+     
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
 
     }
 
