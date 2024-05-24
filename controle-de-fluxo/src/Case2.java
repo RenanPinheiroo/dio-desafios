0d69eb3d91b191609f30939025e673968f5e1aa8
Renan <renan.economia.2011@gmail.com>
2024-05-24 09:20:02 -0400

    Inclusao do case 1 no case 2

--- controle-de-fluxo/src/Case2.java
+++ controle-de-fluxo/src/Case2.java
@@ -3,15 +3,28 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Case2 {
     public static void main(String[] args) {
         String[] candidatos = getName();
-        double salary = Math.round(ValorPretendido());
         double salaryCia = 2000;
         int NumeroFuncionarios = 0;
         int ProximoCandidato = 0 ;
        
         while (NumeroFuncionarios < 5 && ProximoCandidato < candidatos.length) {
             String candidato = candidatos[ProximoCandidato++];
+            double salary = Math.round(ValorPretendido());
 
             System.out.println("O Funcionario " + candidato + " deseja ter um salario de " + salary);
+
+            if( salaryCia > salary){
+
+                System.out.println("LIGAR PARA O CANDIDATO!");
+    
+            } else if( salaryCia == salary){
+    
+                System.out.println("LIGAR PARA O CANDIDATO, COM CONTRA PROPOSTA!");
+    
+            } else {
+    
+                System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS!");
+                }
             
         }
     }
