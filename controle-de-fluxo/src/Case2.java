4c8e665bfd669e2e55db381b34842c9c52e5c177
Renan <renan.economia.2011@gmail.com>
2024-05-24 08:52:38 -0400

    Case 2

--- controle-de-fluxo/src/Case2.java
+++ controle-de-fluxo/src/Case2.java
@@ -1,3 +1,18 @@
+
 public class Case2 {
+    public static void main(String[] args) {
+        String[] candidatos = getName();
+
+        System.out.println(candidatos[4]);
+
+    }
     
+    static String[] getName(){
+
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        return candidatos;
+
+    }
+
+
 }
